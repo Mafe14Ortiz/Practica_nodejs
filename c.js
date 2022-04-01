31ced3b23f6a941bbd3743ddc2ea3cb8c6f7562c
oscarparra0 <oscarivan.parraflorian@gmail.com>
2022-03-31 20:29:24 -0400

    implementacion de objetos

--- c.js
+++ c.js
@@ -5,4 +5,23 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/spanish', function (req, res) {
+    res.send('Hola mundo')
+  })
+
+app.get('/Korean', function (req, res) {
+    res.send('홀라 문도')
+  })
+
+app.get('/Persian', function (req, res) {
+    res.send('هولا موندو')
+  })
+
+  app.get('/Swedish', function (req, res) {
+    res.send('Hej Mundo')
+  })  
+  app.get('/Tailandes', function (req, res) {
+    res.send('สวัสดีชาวโลก')
+  })  
+  
 app.listen(3000)
